67684865e91224f2898a7a84df59d8e7004f486b
Jan Stamer <j.stamer@pe-international.com>
2012-02-01 09:29:12 +0100

    dfg

---
+++ com.pe-international.sample.datajpa/src/com/pe_international/sample/datajpa/ProcessRepository.java
@@ -0,0 +1,7 @@
+package com.pe_international.sample.datajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcessRepository extends JpaRepository<String, String> {
+
+}
