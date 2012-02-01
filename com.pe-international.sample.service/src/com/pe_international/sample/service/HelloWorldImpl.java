71c9e203d21d2a132344948730b2cdad27f91805
Jan Stamer <j.stamer@pe-international.com>
2012-02-01 16:03:43 +0100

    Cleanup

--- com.pe-international.sample.service/src/com/pe_international/sample/service/HelloWorldImpl.java
+++ com.pe-international.sample.service/src/com/pe_international/sample/service/HelloWorldImpl.java
@@ -1,31 +1,8 @@
 package com.pe_international.sample.service;
 
-import javax.persistence.EntityManagerFactory;
-
-import org.springframework.orm.jpa.JpaTransactionManager;
 
 public class HelloWorldImpl implements HelloWorld {
 	
-	private EntityManagerFactory entityManagerFactory;
-	
-//	private JpaTransactionManager transactionManager;
-//
-//	public JpaTransactionManager getTransactionManager() {
-//		return transactionManager;
-//	}
-//
-//	public void setTransactionManager(JpaTransactionManager transactionManager) {
-//		this.transactionManager = transactionManager;
-//	}
-
-	public EntityManagerFactory getEntityManagerFactory() {
-		return entityManagerFactory;
-	}
-
-	public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
-		this.entityManagerFactory = entityManagerFactory;
-	}
-
 	@Override
 	public void sayHello() {
 		System.out.println("Hello World");
